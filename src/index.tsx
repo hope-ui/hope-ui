b5106dd18423186fa1242c879882fe4133eff043
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2022-01-10 12:14:15 +0100

    chore: add commitlint, config typescript, eslint, etc

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import { render } from "solid-js/web";
 
-import { globalStyles } from "./lib/stitches/globalStyles";
 import App from "./App";
 
-render(() => {
-  globalStyles();
-  return <App />;
-}, document.getElementById("root") as HTMLElement);
+render(() => <App />, document.getElementById("root") as HTMLElement);
