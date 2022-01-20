1ef9af3af007de764ca88f1c7317439f9bdc31bb
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2022-01-20 17:35:01 +0100

    fix: fix bug variant prop + typescript support

--- src/components/factory.tsx
+++ src/components/factory.tsx
@@ -19,20 +19,43 @@ function styled<
 >(component: C, ...composers: CSSComposer<Composers>): HopeComponent<C, Composers> {
   const cssComponent = css(...(composers as CSSComposer<Composers> & string));
 
-  const styledComponent = <T extends ElementType = C>(props: HopeComponentProps<T, Composers>) => {
-    const stylePropNames = getIntersectionKeys(props, stylePropsConfig);
+  /**
+   * Get an array of all stitches variant name related to that component creation.
+   * This run only once when calling `hope("tag", {})`.
+   */
+  const variantPropNames = composers.flatMap(composer =>
+    composer.variants ? Object.keys(composer.variants) : []
+  );
 
-    const [local, styleProps, others] = splitProps(props, commonPropNames, stylePropNames);
+  /**
+   * The Generated styled component function that is style props aware.
+   */
+  const styledComponent = <T extends ElementType = C>(props: HopeComponentProps<T, Composers>) => {
+    /**
+     * Get an array of prop names that are only used style props and doesn't have same name as a variant props.
+     * This will ensure we pass the small array possible to `splitProps()`
+     */
+    const usedStylePropNames = getIntersectionKeys(props, stylePropsConfig).filter(
+      name => !variantPropNames.includes(name)
+    );
+
+    const [local, variantProps, styleProps, others] = splitProps(
+      props,
+      commonPropNames,
+      variantPropNames,
+      usedStylePropNames
+    );
 
     const className = () => {
       const cssComponentClassName = cssComponent({
+        ...variantProps, // variants are first class citizen in stitches api
         css: {
-          ...styleProps,
-          ...local.css,
+          ...styleProps, // style props are basically syntactic sugared css overrides
+          ...local.css, // This should be last to ensure the `css` prop override style props
         },
       });
 
-      return `${cssComponentClassName} ${local.className}`;
+      return `${cssComponentClassName} ${local.className ?? ""}`.trim();
     };
 
     return <Dynamic component={local.as ?? component} className={className()} {...others} />;
