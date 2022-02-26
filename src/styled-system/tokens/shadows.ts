7d4d96d0332213da1a2909f8568f6d5e8921dd8c
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2022-02-26 15:46:06 +0100

    feat: change inner shadow token

--- src/styled-system/tokens/shadows.ts
+++ src/styled-system/tokens/shadows.ts
@@ -6,7 +6,7 @@ export const shadows = {
   lg: "0 10px 15px -3px rgb(0 0 0 / 0.09), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
   xl: "0 20px 25px -5px rgb(0 0 0 / 0.09), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
   "2xl": "0 25px 50px -12px rgb(0 0 0 / 0.24)",
-  inner: "inset 0 3px 6px 0 rgb(0 0 0 / 0.4)",
+  inner: "inset 0 2px 4px 0 rgb(0 0 0 / 0.06)",
 };
 
 export const darkShadows = {
