57c0170d2bfb5356bcfa0ace1350aa960698b5c1
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2022-03-06 18:36:20 +0100

    fix: checbox svg icon size

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -125,16 +125,14 @@ const hopeCheckboxLabelClass = "hope-checkbox__label";
 const CheckboxIconCheck = createIcon({
   viewBox: "0 0 15 15",
   path: () => (
-    <g fill="none">
-      <path
-        d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
-        fill="currentColor"
-        stroke="currentColor"
-        stroke-width="1"
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-      ></path>
-    </g>
+    <path
+      d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
+      fill="currentColor"
+      stroke="currentColor"
+      stroke-width="1"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+    />
   ),
 });
 
@@ -142,16 +140,14 @@ const CheckboxIconCheck = createIcon({
 const CheckboxIconIndeterminate = createIcon({
   viewBox: "0 0 15 15",
   path: () => (
-    <g fill="none">
-      <path
-        d="M2.25 7.5C2.25 7.22386 2.47386 7 2.75 7H12.25C12.5261 7 12.75 7.22386 12.75 7.5C12.75 7.77614 12.5261 8 12.25 8H2.75C2.47386 8 2.25 7.77614 2.25 7.5Z"
-        fill="currentColor"
-        stroke="currentColor"
-        stroke-width="1"
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-      ></path>
-    </g>
+    <path
+      d="M2.25 7.5C2.25 7.22386 2.47386 7 2.75 7H12.25C12.5261 7 12.75 7.22386 12.75 7.5C12.75 7.77614 12.5261 8 12.25 8H2.75C2.47386 8 2.25 7.77614 2.25 7.5Z"
+      fill="currentColor"
+      stroke="currentColor"
+      stroke-width="1"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+    />
   ),
 });
 
@@ -161,8 +157,8 @@ export function Checkbox<C extends ElementType = "label">(props: CheckboxProps<C
   const defaultProps: CheckboxProps<"label"> = {
     as: "label",
     id: `hope-checkbox-${createUniqueId()}`,
-    iconChecked: <CheckboxIconCheck />,
-    iconIndeterminate: <CheckboxIconIndeterminate />,
+    iconChecked: <CheckboxIconCheck boxSize="$full" />,
+    iconIndeterminate: <CheckboxIconIndeterminate boxSize="$full" />,
     variant: theme?.defaultProps?.root?.variant ?? "outline",
     colorScheme: theme?.defaultProps?.root?.colorScheme ?? "primary",
     size: theme?.defaultProps?.root?.size ?? "md",
