4f5c34c0abf3843c0899b7a619e525df2a11fedc
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2022-03-22 08:56:03 +0100

    fix(button-group): zIndex issue on focused button

--- src/playground.tsx
+++ src/playground.tsx
@@ -7,10 +7,14 @@ import {
   Box,
   Button,
   ButtonGroup,
+  FormControl,
+  FormLabel,
   HopeProvider,
   HopeThemeConfig,
   HStack,
   IconButton,
+  Radio,
+  RadioGroup,
   SimpleOption,
   SimpleSelect,
   useColorMode,
@@ -21,28 +25,31 @@ import { IconCheck } from "./components/icons/IconCheck";
 export function App() {
   const { toggleColorMode } = useColorMode();
 
+  const [gender, setGender] = createSignal<string>();
+
   return (
     <Box p="$4">
       <HStack spacing="$4" mb="$4">
         <Button variant="subtle" colorScheme="neutral" onClick={toggleColorMode}>
           Toggle color mode
         </Button>
+        <Button></Button>
+        <ButtonGroup size="sm" attached variant="outline">
+          <Button mr="-1px">Save</Button>
+          <IconButton aria-label="Add to friends" icon={<IconCheck />} />
+        </ButtonGroup>
       </HStack>
       <VStack spacing="$4" alignItems="flex-start">
-        <SimpleSelect placeholder="Choose wisely" maxW={300}>
-          <SimpleOption value="react">React</SimpleOption>
-          <SimpleOption value="angular">Angular</SimpleOption>
-          <SimpleOption value="vue">Vue</SimpleOption>
-          <SimpleOption value="svelte">Svelte</SimpleOption>
-          <SimpleOption value="solid">Solid</SimpleOption>
-        </SimpleSelect>
-        <SimpleSelect multiple placeholder="Choose wisely" maxW={300}>
-          <SimpleOption value="react">React</SimpleOption>
-          <SimpleOption value="angular">Angular</SimpleOption>
-          <SimpleOption value="vue">Vue</SimpleOption>
-          <SimpleOption value="svelte">Svelte</SimpleOption>
-          <SimpleOption value="solid">Solid</SimpleOption>
-        </SimpleSelect>
+        <FormControl>
+          <FormLabel>Gender</FormLabel>
+          <RadioGroup name="gender" value={gender()} onChange={setGender}>
+            <HStack spacing="$5">
+              <Radio value="male">Male</Radio>
+              <Radio value="female">Female</Radio>
+              <Radio value="other">Other</Radio>
+            </HStack>
+          </RadioGroup>
+        </FormControl>
       </VStack>
     </Box>
   );
