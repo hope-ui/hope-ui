47f2b41462d359c24bcf87512fbf51311a52856d
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2022-04-06 00:13:55 +0200

    chore: rollback to vite library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,40 @@
+import path from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import solidPlugin from "vite-plugin-solid";
 
+import pkg from "./package.json";
+
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    dts({
+      tsConfigFilePath: "tsconfig.build.json",
+      insertTypesEntry: true,
+      noEmitOnError: true,
+      skipDiagnostics: false,
+      logDiagnostics: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs"],
+      fileName: format => (format === "es" ? "index.mjs" : "index.cjs"),
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies),
+        ...Object.keys(pkg.peerDependencies),
+        "solid-js/web",
+        "solid-js/store",
+      ],
     },
   },
 });
