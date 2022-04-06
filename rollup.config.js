91be746ed72ec547a041fa89c721785c1ef675e8
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2022-04-06 09:52:48 +0200

    fix: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
 import path from "path";
 import withSolid from "rollup-preset-solid";
 
-import pkg from "./package.json";
-
 export default withSolid({
   input: path.resolve(__dirname, "src/index.ts"),
   targets: ["esm", "cjs"],
-  external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies), "solid-js/web", "solid-js/store"],
 });
