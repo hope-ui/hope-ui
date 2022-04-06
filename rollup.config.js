94d085fc9369d1c27ea711725f8ec88b379f66fd
Fabien MARIE-LOUISE <fabien.marie-louise@symaris.fr>
2022-04-06 10:48:07 +0200

    fix: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
-import path from "path";
 import withSolid from "rollup-preset-solid";
 
 export default withSolid({
-  input: path.resolve(__dirname, "src/index.ts"),
+  input: "./src/index.ts",
   targets: ["esm", "cjs"],
 });
