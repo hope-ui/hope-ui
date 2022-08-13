6d7b8c4a4f549bb40e44ce229dff86b9e32c591e
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-08-13 14:04:18 +0200

    chore: test deploy solid-start to netlify

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,6 +1,10 @@
 import solid from "solid-start/vite";
+import netlify from "solid-start-netlify";
+import node from "solid-start-node";
 import { defineConfig } from "vite";
 
+const adapter = process.env.GITHUB_ACTIONS ? node() : netlify();
+
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
+  plugins: [solid({ adapter })],
 });
