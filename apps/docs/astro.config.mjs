0bd3a6b93a62fb5cb52f50d716093422b6064101
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-08-26 19:16:47 +0200

    chore: bump to solid 1.5

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [solid()],
   site: "https://hope-ui.com",
 });
