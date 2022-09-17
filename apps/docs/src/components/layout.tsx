8a737a890ea7132189bd880940a38170c87a617a
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-09-17 23:55:49 +0200

    chore: release 1.0.0-next.3

--- apps/docs/src/components/layout.tsx
+++ apps/docs/src/components/layout.tsx
@@ -127,7 +127,7 @@ function Header(props: HeaderProps) {
                 color: "neutral.300",
               }}
             >
-              v1.0.0-next.2
+              v1.0.0-next.3
             </hope.span>
           </HStack>
         </Flex>
