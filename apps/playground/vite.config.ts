04c2db800c9008c0e79bb595a47a8682fb270359
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-09-20 17:35:25 +0200

    chore: update deps, cant build solis-start

--- apps/playground/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  plugins: [solid()],
-  ssr: {
-    noExternal: ["@hope-ui/core", "@hope-ui/styles"],
-  },
-});
--- packages/core/dev/App.tsx
+++ packages/core/dev/App.tsx
@@ -1,29 +1,3 @@
-import { Box, useColorMode } from "../src";
-
 export default function App() {
-  const { toggleColorMode } = useColorMode();
-
-  return (
-    <>
-      <button onClick={toggleColorMode}>toggle color mode</button>
-      <Box
-        mb={4}
-        p={{ light: 2, dark: 4 }}
-        bg={{
-          base: { light: "blue", dark: "red" },
-          sm: { light: "tomato", dark: "teal" },
-          md: { light: "green", dark: "yellow" },
-        }}
-        color={[{ light: "black", dark: "white" }, null, { light: "white", dark: "black" }]}
-        _hover={{
-          fontWeight: "bold",
-          _dark: {
-            fontWeight: "hairline",
-          },
-        }}
-      >
-        This box's style will change based on the color mode.
-      </Box>
-    </>
-  );
+  return <></>;
 }
