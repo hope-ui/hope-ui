84f49bccfb2e61e361e67b3aafb15ccc4736b9c6
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-10-31 16:20:44 +0100

    chore(style): rollback to default colors

--- packages/styles/src/theme/create-default-colors.tsx
+++ packages/styles/src/theme/create-default-colors.tsx
@@ -12,128 +12,112 @@
  * https://github.com/chakra-ui/chakra-ui/blob/main/packages/theme/src/foundations/colors.ts
  */
 
-import { PaletteScale, ThemeColors } from "../types";
+import { ThemeColors } from "../types";
 import { createGetCssVar } from "../utils/css-var";
-import { createPalette, createReversePalette, reverseScale } from "./create-palette";
-
-const whiteAlphaScale: PaletteScale = {
-  50: "rgba(255, 255, 255, 0.04)",
-  100: "rgba(255, 255, 255, 0.06)",
-  200: "rgba(255, 255, 255, 0.08)",
-  300: "rgba(255, 255, 255, 0.16)",
-  400: "rgba(255, 255, 255, 0.24)",
-  500: "rgba(255, 255, 255, 0.36)",
-  600: "rgba(255, 255, 255, 0.48)",
-  700: "rgba(255, 255, 255, 0.64)",
-  800: "rgba(255, 255, 255, 0.80)",
-  900: "rgba(255, 255, 255, 0.92)",
-};
-
-const blackAlphaScale: PaletteScale = {
-  50: "rgba(0, 0, 0, 0.04)",
-  100: "rgba(0, 0, 0, 0.06)",
-  200: "rgba(0, 0, 0, 0.08)",
-  300: "rgba(0, 0, 0, 0.16)",
-  400: "rgba(0, 0, 0, 0.24)",
-  500: "rgba(0, 0, 0, 0.36)",
-  600: "rgba(0, 0, 0, 0.48)",
-  700: "rgba(0, 0, 0, 0.64)",
-  800: "rgba(0, 0, 0, 0.80)",
-  900: "rgba(0, 0, 0, 0.92)",
-};
-
-const primaryScale: PaletteScale = {
-  50: "#e6f6ff",
-  100: "#bae3ff",
-  200: "#7cc4fa",
-  300: "#47a3f3",
-  400: "#2186eb",
-  500: "#0967d2",
-  600: "#0552b5",
-  700: "#03449e",
-  800: "#01337d",
-  900: "#002159",
-};
-
-// Tailwind neutral (+ 10% saturation blue - hue 217)
-const neutralScale: PaletteScale = {
-  50: "#f9fafa",
-  100: "#f4f5f6",
-  200: "#e3e5e8",
-  300: "#cfd3d8",
-  400: "#9aa1ac",
-  500: "#67707e",
-  600: "#49505a",
-  700: "#393e46",
-  800: "#22252a",
-  900: "#151619",
-};
-
-const successScale: PaletteScale = {
-  50: "#e3f9e5",
-  100: "#c1eac5",
-  200: "#a3d9a5",
-  300: "#7bc47f",
-  400: "#57ae5b",
-  500: "#3f9142",
-  600: "#2f8132",
-  700: "#207227",
-  800: "#0e5814",
-  900: "#05400a",
-};
-
-const infoScale: PaletteScale = {
-  50: "#eae2f8",
-  100: "#cfbcf2",
-  200: "#a081d9",
-  300: "#8662c7",
-  400: "#724bb7",
-  500: "#653cad",
-  600: "#51279b",
-  700: "#421987",
-  800: "#34126f",
-  900: "#240754",
-};
-
-const warningScale: PaletteScale = {
-  50: "#fffbea",
-  100: "#fff3c4",
-  200: "#fce588",
-  300: "#fadb5f",
-  400: "#f7c948",
-  500: "#f0b429",
-  600: "#de911d",
-  700: "#cb6e17",
-  800: "#b44d12",
-  900: "#8d2b0b",
-};
-
-const dangerScale: PaletteScale = {
-  50: "#ffe3e3",
-  100: "#ffbdbd",
-  200: "#ff9b9b",
-  300: "#f86a6a",
-  400: "#ef4e4e",
-  500: "#e12d39",
-  600: "#cf1124",
-  700: "#ab091e",
-  800: "#8a041a",
-  900: "#610316",
-};
+import { createPalette } from "./create-palette";
 
 export function createDefaultColors(cssVarPrefix: string): ThemeColors {
   const getCssVar = createGetCssVar(cssVarPrefix);
 
   return {
     light: {
-      whiteAlpha: whiteAlphaScale,
-      blackAlpha: blackAlphaScale,
-      primary: createPalette(primaryScale),
-      neutral: createPalette(neutralScale),
-      success: createPalette(successScale),
-      info: createPalette(infoScale),
-      warning: createPalette(warningScale),
-      danger: createPalette(dangerScale),
+      whiteAlpha: {
+        50: "rgba(255, 255, 255, 0.04)",
+        100: "rgba(255, 255, 255, 0.06)",
+        200: "rgba(255, 255, 255, 0.08)",
+        300: "rgba(255, 255, 255, 0.16)",
+        400: "rgba(255, 255, 255, 0.24)",
+        500: "rgba(255, 255, 255, 0.36)",
+        600: "rgba(255, 255, 255, 0.48)",
+        700: "rgba(255, 255, 255, 0.64)",
+        800: "rgba(255, 255, 255, 0.80)",
+        900: "rgba(255, 255, 255, 0.92)",
+      },
+      blackAlpha: {
+        50: "rgba(0, 0, 0, 0.04)",
+        100: "rgba(0, 0, 0, 0.06)",
+        200: "rgba(0, 0, 0, 0.08)",
+        300: "rgba(0, 0, 0, 0.16)",
+        400: "rgba(0, 0, 0, 0.24)",
+        500: "rgba(0, 0, 0, 0.36)",
+        600: "rgba(0, 0, 0, 0.48)",
+        700: "rgba(0, 0, 0, 0.64)",
+        800: "rgba(0, 0, 0, 0.80)",
+        900: "rgba(0, 0, 0, 0.92)",
+      },
+      primary: createPalette({
+        50: "#e6f6ff",
+        100: "#bae3ff",
+        200: "#7cc4fa",
+        300: "#47a3f3",
+        400: "#2186eb",
+        500: "#0967d2",
+        600: "#0552b5",
+        700: "#03449e",
+        800: "#01337d",
+        900: "#002159",
+      }),
+      // Tailwind neutral (+ 10% saturation blue - hue 217)
+      neutral: createPalette({
+        50: "#f9fafa",
+        100: "#f4f5f6",
+        200: "#e3e5e8",
+        300: "#cfd3d8",
+        400: "#9aa1ac",
+        500: "#67707e",
+        600: "#49505a",
+        700: "#393e46",
+        800: "#22252a",
+        900: "#151619",
+      }),
+      success: createPalette({
+        50: "#e3f9e5",
+        100: "#c1eac5",
+        200: "#a3d9a5",
+        300: "#7bc47f",
+        400: "#57ae5b",
+        500: "#3f9142",
+        600: "#2f8132",
+        700: "#207227",
+        800: "#0e5814",
+        900: "#05400a",
+      }),
+      info: createPalette({
+        50: "#eae2f8",
+        100: "#cfbcf2",
+        200: "#a081d9",
+        300: "#8662c7",
+        400: "#724bb7",
+        500: "#653cad",
+        600: "#51279b",
+        700: "#421987",
+        800: "#34126f",
+        900: "#240754",
+      }),
+      warning: createPalette({
+        50: "#fffbea",
+        100: "#fff3c4",
+        200: "#fce588",
+        300: "#fadb5f",
+        400: "#f7c948",
+        500: "#f0b429",
+        600: "#de911d",
+        700: "#cb6e17",
+        800: "#b44d12",
+        900: "#8d2b0b",
+      }),
+      danger: createPalette({
+        50: "#ffe3e3",
+        100: "#ffbdbd",
+        200: "#ff9b9b",
+        300: "#f86a6a",
+        400: "#ef4e4e",
+        500: "#e12d39",
+        600: "#cf1124",
+        700: "#ab091e",
+        800: "#8a041a",
+        900: "#610316",
+      }),
       common: {
         white: "#ffffff",
         black: "#121212", // very dark neutral gray, do not use pure black.,
@@ -143,16 +127,6 @@ export function createDefaultColors(cssVarPrefix: string): ThemeColors {
       },
     },
     dark: {
-      /*
-      whiteAlpha: reverseScale(whiteAlphaScale),
-      blackAlpha: reverseScale(blackAlphaScale),
-      primary: createReversePalette(primaryScale),
-      neutral: createReversePalette(neutralScale),
-      success: createReversePalette(successScale),
-      info: createReversePalette(infoScale),
-      warning: createReversePalette(warningScale),
-      danger: createReversePalette(dangerScale),
-      */
       common: {
         foreground: getCssVar("colors-neutral-200"),
         background: getCssVar("colors-neutral-900"),
